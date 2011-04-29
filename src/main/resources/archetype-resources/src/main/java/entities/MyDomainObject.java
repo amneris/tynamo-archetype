0635e5b6d36134238671968040d430285e182c6c
ascandroli <ascandroli@amneris.es>
2011-04-29 14:55:24 +0200

    adding a simple REST example

--- src/main/resources/archetype-resources/src/main/java/entities/MyDomainObject.java
+++ src/main/resources/archetype-resources/src/main/java/entities/MyDomainObject.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@DefaultBeanModel(reorder = "id")
+@XmlRootElement(name = "mydomainobject")
 public class MyDomainObject
 {
 	private Long id;
