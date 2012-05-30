654ff12a8a5cc2f9b18da13c4d18c509390e32fb
Alejandro Scandroli <ascandroli@amneris.eu>
2012-05-30 13:11:03 +0200

    ohhhhh what a typo!

--- src/main/resources/archetype-resources/src/main/java/components/MenuItem.java
+++ src/main/resources/archetype-resources/src/main/java/components/MenuItem.java
@@ -15,73 +15,73 @@ import java.util.regex.Pattern;
 public class MenuItem extends AbstractLink
 {
 
-    private static final String CONTEXT = "context";
+	private static final String CONTEXT = "context";
 
-    private static final String SELECTED = "selected";
+	private static final String SELECTED = "selected";
 
-    private static final String CLASS = "class";
+	private static final String CLASS = "class";
 
-    private static final String LI = "li";
+	private static final String LI = "li";
 
-    @Inject
-    private ComponentResources resources;
+	@Inject
+	private ComponentResources resources;
 
-    @Inject
-    private PageRenderLinkSource linkSource;
+	@Inject
+	private PageRenderLinkSource linkSource;
 
-    @Parameter
-    private Object[] context;
+	@Parameter
+	private Object[] context;
 
-    @Parameter(required = true, allowNull = false, defaultPrefix = BindingConstants.LITERAL)
-    private String page;
+	@Parameter(required = true, allowNull = false, defaultPrefix = BindingConstants.LITERAL)
+	private String page;
 
-    @Parameter(required = false, defaultPrefix = BindingConstants.LITERAL)
-    private String expression;
+	@Parameter(required = false, defaultPrefix = BindingConstants.LITERAL)
+	private String expression;
 
-    @Inject
-    private Request request;
+	@Inject
+	private Request request;
 
-    void beginRender(MarkupWriter writer)
-    {
-        final Link link;
+	void beginRender(MarkupWriter writer)
+	{
+		final Link link;
 
-        if (resources.isBound(CONTEXT))
-        {
-            link = linkSource.createPageRenderLinkWithContext(page, context);
-        } else
-        {
-            link = linkSource.createPageRenderLink(page);
-        }
+		if (resources.isBound(CONTEXT))
+		{
+			link = linkSource.createPageRenderLinkWithContext(page, context);
+		} else
+		{
+			link = linkSource.createPageRenderLink(page);
+		}
 
-        if (isSelected())
-        {
-            writer.element(LI, CLASS, SELECTED);
-            writeLink(writer, link, CLASS, SELECTED);
-        } else
-        {
-            writer.element(LI);
-            writeLink(writer, link);
-        }
-    }
+		if (isSelected())
+		{
+			writer.element(LI, CLASS, SELECTED);
+			writeLink(writer, link, CLASS, SELECTED);
+		} else
+		{
+			writer.element(LI);
+			writeLink(writer, link);
+		}
+	}
 
-    private boolean isSelected()
-    {
-        final String pageName = resources.getPageName();
-        boolean selected = pageName.equalsIgnoreCase(page);
+	private boolean isSelected()
+	{
+		final String pageName = resources.getPageName();
+		boolean selected = pageName.equalsIgnoreCase(page);
 
-        if (resources.isBound(expression))
-        {
-            String path = request.getPath();
-            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-            selected = pattern.matcher(path).matches();
-        }
+		if (resources.isBound("expression"))
+		{
+			String path = request.getPath();
+			Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+			selected = pattern.matcher(path).matches();
+		}
 
-        return selected;
-    }
+		return selected;
+	}
 
-    void afterRender(MarkupWriter writer)
-    {
-        writer.end(); // <a>
-        writer.end(); // <li>
-    }
+	void afterRender(MarkupWriter writer)
+	{
+		writer.end(); // <a>
+		writer.end(); // <li>
+	}
 }
