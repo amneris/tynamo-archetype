5e0b4067d299522255f279581043d8fd3b73266c
Alejandro Scandroli <ascandroli@amneris.eu>
2013-01-24 11:08:51 +0100

    fix a comment

--- src/main/resources/archetype-resources/src/main/java/services/AppModule.java
+++ src/main/resources/archetype-resources/src/main/java/services/AppModule.java
@@ -87,7 +87,7 @@ public class AppModule
 
 	/**
 	 * By default tapestry-hibernate will scan
-	 * InternalConstants.TAPESTRY_APP_PACKAGE_PARAM + ".entities" (witch is equal to "${package}.${artifactId}.entities")
+	 * InternalConstants.TAPESTRY_APP_PACKAGE_PARAM + ".entities" (witch is equal to "${package}.entities")
 	 * for annotated entity classes.
 	 *
 	 * Contributes the package "${package}.${artifactId}.model" to the configuration, so that it will be
